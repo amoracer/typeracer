3bc20851ae85910df15fdc32ed1fd68b85e66dc0
Kas <kasrasenberg@gmail.com>
2019-09-05 11:13:38 +0200

    probeer maar lukt niet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,44 @@
                     @endif
 
                     Welkom bij TypeRacer
+                    <p id="word">Loading...</p>
+                    <br><input type='text' oninput="readtext()" id='typeracerinput'>
+                    <p id="output">Your input</p>
+                    <p id="checker">Your answer is correct/incorrect</p>
+                    <script>
+                        
+                    $(document).keyup(function(event) {
+                    if ($("typeracerinput").is(":focus") && event.key == "Enter") {
+                        alert('ya!');
+                    }
+                    });
+                    
+                    function readtext() {
+                        $(".input1").on('keyup', function (e) {
+                        if (e.keyCode === 13) {
+        // Do something
+                    var input = document.getElementById("typeracerinput").value;
+                    //document.getElementById("output").innerHTML = input;   
+                    var counter     
+                    var word = 'test';
+                    var result = input.includes(word);
+                    if(result == true){
+                        document.getElementById("output").innerHTML = "<p style='color:green'>Your answer is correct</p>"
+                        document.getElementById("typeracerinput").value = "";
+                        result = false;
+                        counter += 1;
+                    }
+                    if(result == false){
+                        document.getElementById("output").innerHTML = "<p style='color:red'>Your answer is incorrect</p>"
+                    }
+                    
+                    }
+                    }
+                    }  
+                        	}
+});
+
+                    </script>
                 </div>
             </div>
         </div>
